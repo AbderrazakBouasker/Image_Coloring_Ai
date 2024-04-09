2dd563810162dcf7632e810a5eac0cc47bac6ad8
AbderrazakBouasker <73231563+AbderrazakBouasker@users.noreply.github.com>
2024-04-09 13:44:14 +0000

    main

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+import json
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return render_template("index.html", title="Hello")
+
+@app.get("/results")
+def results():
+    # Load the JSON file containing images and image URLs
+    with open("data.json") as file:
+        data = json.load(file)
+        print("test")
+    return render_template("results.html", images=data)
+
+@app.post("/colorize")
+def colorize():
+    return 
