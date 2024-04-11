51106fd1dc33c93202efb62584f63d30badee101
JoseMokeni <94537496+JoseMokeni@users.noreply.github.com>
2024-04-11 23:34:26 +0000

    credentials excluded maybe

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Image_Coloring_Ai
 a platform for an image coloring deeplearning model
+
+Execute this bash command to add credentials file to env variables (just a temporary solution)
+```bash
+export GOOGLE_APPLICATION_CREDENTIALS="/workspaces/Image_Coloring_Ai/credentials.json"
+```
---
+++ upload.py
@@ -0,0 +1,26 @@
+# this script will define a function to upload a file to a gcloud bucket
+# the credentials file is stored in the root directory of the project
+
+import os
+from google.cloud import storage
+from google.oauth2 import service_account
+
+def upload_blob(bucket_name, source_file_name, destination_blob_name):
+    """Uploads a file to the bucket."""
+    # credentials file
+    credentials = service_account.Credentials.from_service_account_file('credentials.json')
+    storage_client = storage.Client(credentials=credentials)
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(destination_blob_name)
+
+    blob.upload_from_filename(source_file_name)
+
+    print(
+        "File {} uploaded to {}.".format(
+            source_file_name, destination_blob_name
+        )
+    )
+    return blob.public_url # return the public url of the file
+
+# test the function
+print(upload_blob('image_coloring_bucket', 'image.jpg', 'imageUpload.jpg'))
